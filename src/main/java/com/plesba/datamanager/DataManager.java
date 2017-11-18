6e686b845d717a9f82a1c76d31d107582c53f672
Renee Damm <renee@plesba.com>
2017-11-18 15:03:09 -0800

    latest

--- src/main/java/com/plesba/datamanager/DataManager.java
+++ src/main/java/com/plesba/datamanager/DataManager.java
@@ -6,8 +6,9 @@
 package com.plesba.datamanager;
 
 import com.plesba.datamanager.utils.DBConnection;
-import com.plesba.datamanager.utils.DBProperties;
+import com.plesba.datamanager.utils.DMProperties;
 import java.io.IOException;
+import java.util.Properties;
 
 /**
  *
@@ -15,30 +16,38 @@ import java.io.IOException;
  */
 public class DataManager {
     
-    
-    DBProperties dbProps;
-    DBConnection dbConnection;
-    
-public static void main(String[] args) throws IOException {
+        private static String propertiesFile = null;
+        private static Properties dataMgrProps = null;
+        private static DBConnection dbConnection = null;
+        
+    public static void main(String[] args) throws IOException {
 
-        System.out.println("Starting DataManager main");
-        String propertiesFile = null;
-        String databasePropertiesFile = null;
-        if (args.length < 1) {
+        System.out.println("Starting DataManager main........");
+
+        if (args.length == 1) {
+            propertiesFile = args[0];
+            System.out.println("Properties file: " + propertiesFile);
+        } else {
             System.err.println(" <propertiesFile>" + "Usage: java " + DataManager.class.getName());
             System.exit(1);
-        } else if (args.length == 1) {
-            propertiesFile = args[0];
-            System.out.println("Properties file: " + propertiesFile);
-        } else if (args.length == 2) {
-            propertiesFile = args[0];
-            System.out.println("Properties file: " + propertiesFile);
-            databasePropertiesFile = args[1];
-            System.out.println("Database Properties file: " + databasePropertiesFile);
         }
-         
-        dbProps = new DBProperties(databasePropertiesFile);
-        dbConnection = new DBConnection(dpProps);
 
+        dataMgrProps = new DMProperties(propertiesFile).getProp();
+        dbConnection = getConnection(); 
+        System.out.println(dbConnection.getConnectString());
+        
+
+    }
+    public static DBConnection getConnection(){
+    
+        return new DBConnection.ConnectionBuilder()
+                .user(dataMgrProps.getProperty("database.user"))
+                .password(dataMgrProps.getProperty("database.password"))
+                .database(dataMgrProps.getProperty("database.database"))
+                .port(dataMgrProps.getProperty("database.port"))
+                .driver(dataMgrProps.getProperty("database.driver"))
+                .host(dataMgrProps.getProperty("database.host"))
+                .build();
+    }   
+      
 }
-
